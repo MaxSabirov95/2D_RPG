bf6d2a12c837f647569f336a0278da63180a71c6
MYoshua64 <MYoshua64@users.noreply.github.com>
2020-05-29 19:57:49 +0300

    Quest Giver and Camera

---
+++ Test2DProject/Assets/MultiplayerCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+namespace Max_Almog.MyCompany.MyGame
+{
+    public class MultiplayerCam : MonoBehaviour
+    {
+        CinemachineVirtualCamera thisCamera;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            thisCamera = GetComponent<CinemachineVirtualCamera>();
+            thisCamera.Follow = PlayerMovement.LocalPlayerInstance.transform;
+        }
+    }
+}
