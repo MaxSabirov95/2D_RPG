7d8661aca3945dad7df2b32cacb3b701504dbe95
MaxSabirov <53007578+MaxSabirov@users.noreply.github.com>
2020-06-24 21:04:20 +0300

    vv

--- Test2DProject/Assets/Scripts/ItemsPickUp.cs
+++ Test2DProject/Assets/Scripts/ItemsPickUp.cs
@@ -29,6 +29,7 @@ namespace Max_Almog.MyCompany.MyGame
 
         void Start()
         {
+            inventory = null;
             Player = null;
             Physics2D.IgnoreLayerCollision(10, 9);
             rb = GetComponent<Rigidbody2D>();
