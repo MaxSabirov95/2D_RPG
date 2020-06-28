8061d81f59155c3e318114b1657249c004f35823
MaxSabirov <53007578+MaxSabirov@users.noreply.github.com>
2020-06-28 10:14:07 +0300

    brr

--- Test2DProject/Assets/Scripts/Enemy.cs
+++ Test2DProject/Assets/Scripts/Enemy.cs
@@ -117,7 +117,7 @@ namespace Max_Almog.MyCompany.MyGame
         [PunRPC]
         public void Die()
         {
-            BlackBoard.spawnerInstance.photonView.RPC("FlagEnemyDeath", RpcTarget.AllBuffered,new object[] { this});
+            BlackBoard.spawnerInstance.photonView.RPC("FlagEnemyDeath", RpcTarget.AllBuffered, );
             int ID = gameObject.GetComponent<PhotonView>().ViewID;
             Destroy(PhotonView.Find(ID).gameObject);
         }
